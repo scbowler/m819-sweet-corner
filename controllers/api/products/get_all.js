6d59585e60baa0088459954cb326249b443e8c1e
Scott Bowler <scott.bowler@learningfuze.com>
2019-09-25 03:47:06 +0000

    Setup default error handler and added error handling to existing endpoints

--- controllers/api/products/get_all.js
+++ controllers/api/products/get_all.js
@@ -2,36 +2,40 @@ const db = require('../../../db');
 const { imageUrl } = require('../../../helpers');
 
 module.exports = async (req, res, next) => {
-    //  Data, Field Data
-    // [  [],  []  ]
-    const [results] = await db.query(`
-        SELECT p.pid AS id, p.caption, p.cost, p.name, i.file, i.altText, i.type, i.pid AS imageId 
-        FROM products AS p 
-        JOIN images AS i 
-        ON i.productId=p.id 
-        WHERE i.type="thumbnail"
-    `);
+    try {
+        //  Data, Field Data
+        // [  [],  []  ]
+        const [results] = await db.query(`
+            SELECT p.pid AS id, p.caption, p.cost, p.name, i.file, i.altText, i.type, i.pid AS imageId 
+            FROM products AS p 
+            JOIN images AS i 
+            ON i.productId=p.id 
+            WHERE i.type="thumbnail"
+        `);
 
-    const products = results.map(p => {
-        const product = {
-            id: p.id,
-            caption: p.caption,
-            cost: p.cost,
-            name: p.name,
-            thumbnail: {
-                id: p.imageId,
-                altText: p.altText,
-                file: p.file,
-                type: p.type,
-                url: imageUrl(req, p.type, p.file)
-            }
-        };
+        const products = results.map(p => {
+            const product = {
+                id: p.id,
+                caption: p.caption,
+                cost: p.cost,
+                name: p.name,
+                thumbnail: {
+                    id: p.imageId,
+                    altText: p.altText,
+                    file: p.file,
+                    type: p.type,
+                    url: imageUrl(req, p.type, p.file)
+                }
+            };
 
-        return product;
-    });
+            return product;
+        });
 
 
-    res.send({
-        products: products
-    });
+        res.send({
+            products: products
+        });
+    } catch(err){
+        next(err);
+    }
 }
