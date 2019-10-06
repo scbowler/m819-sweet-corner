1f0a6e197a57cecf5d1f2368a075f247226ea502
Scott Bowler <scott.bowler@learningfuze.com>
2019-10-06 00:58:27 +0000

    Added client and npm config for client

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const StatusError = require('./helpers/status_error');
 const defaultErrorHandler = require('./middleware/default_error_handler');
 const PORT = process.env.PORT || 9000;
@@ -7,6 +8,7 @@ global.StatusError = StatusError;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
